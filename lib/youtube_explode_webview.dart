d1f83c126b8cc4a4c6374501f15e29e8b1956b64
Mattia <hexer504@pm.me>
2025-11-11 13:09:55 +0100

    initial commit

---
+++ lib/youtube_explode_webview.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:youtube_explode_dart/js_challenge.dart';
+import 'package:youtube_explode_dart/src/reverse_engineering/challenges/ejs/ejs.dart';
+
+class WebviewEJSSolver extends BaseEJSSolver {
+  final WebViewController _controller;
+
+  WebviewEJSSolver(this._controller);
+
+  static Future<WebviewEJSSolver> init() async {
+    final controller = WebViewController();
+    controller.setJavaScriptMode(JavaScriptMode.unrestricted);
+    final modules = await EJSBuilder.getJSModules();
+    controller.runJavaScript(modules);
+
+    return WebviewEJSSolver(controller);
+  }
+
+  @override
+  Future<String> executeJavaScript(String jsCode) async {
+    final result = await _controller.runJavaScriptReturningResult(jsCode);
+    if (result is String) {
+      return json.decode(result);
+    } else {
+      throw Exception(
+        'Expected String result from WebView JS execution, got: $result',
+      );
+    }
+  }
+}
